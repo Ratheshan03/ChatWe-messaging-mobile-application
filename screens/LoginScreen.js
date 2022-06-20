8a85be6b9ce242e01839274027524816e02eda5d
99x-dgCandidate-Ratheshan-1642831919908-at-675482622082 <ratheshan.20200170@iit.ac.lk>
2022-06-20 23:58:35 +0530

    dependencies added

--- screens/LoginScreen.js
+++ screens/LoginScreen.js
@@ -1,10 +1,18 @@
-import { View, Text } from "react-native";
+import { StyleSheet, View, Text } from "react-native";
 import React from "react";
+import { Button, Input, Image } from "@rneui/base";
+import { StatusBar } from "expo-status-bar";
 
 const LoginScreen = () => {
   return (
     <View>
-      <Text>LoginScreen</Text>
+      <StatusBar style="light" />
+      <Image
+        source={{
+          uri: "",
+        }}
+        style={{ width: 200, height: 200 }}
+      />
     </View>
   );
 };
