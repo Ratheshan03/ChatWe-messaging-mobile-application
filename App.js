9a280576706764ff8eb0980e517b9b09bcd45f73
99x-dgCandidate-Ratheshan-1642831919908-at-675482622082 <ratheshan.20200170@iit.ac.lk>
2022-06-20 23:10:09 +0530

    Login screen prep

--- App.js
+++ App.js
@@ -1,12 +1,24 @@
+import "react-native-gesture-handler";
+import * as React from "react";
 import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View } from "react-native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+
+const Stack = createNativeStackNavigator();
+
+const globalsScreenOptions = {
+  headerStyle: { backgroundColor: "#2C6BED" },
+  headerTitleStyle: { color: "white" },
+  headerTintColor: "white",
+};
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Hi Rathe!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator screenOptions={globalsScreenOptions}>
+        <Stack.Screen name="Login" component={LoginScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
 
