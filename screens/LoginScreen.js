634f210e621068fa06c98c49121d56a3966f3de5
99x-dgCandidate-Ratheshan-1642831919908-at-675482622082 <ratheshan.20200170@iit.ac.lk>
2022-06-21 23:11:34 +0530

    fixed errors

--- screens/LoginScreen.js
+++ screens/LoginScreen.js
@@ -9,7 +9,7 @@ const LoginScreen = () => {
       <StatusBar style="light" />
       <Image
         source={{
-          uri: "",
+          uri: "https://blog.mozilla.org/internetcitizen/files/2018/08/signal-logo.png",
         }}
         style={{ width: 200, height: 200 }}
       />
